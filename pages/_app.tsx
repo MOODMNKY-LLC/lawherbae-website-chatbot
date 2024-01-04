afdf943e309282a4ee4f5d274e5640c819389676
MOODMNKY-LLC <simeon.bowman@moodmnky.com>
2024-01-04 23:13:55 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ pages/_app.tsx
@@ -0,0 +1,110 @@
+// global styles shared across the entire site
+import * as React from 'react'
+import type { AppProps } from 'next/app'
+import { useRouter } from 'next/router'
+import { BubbleChat } from 'flowise-embed-react'; // Import BubbleChat
+
+import * as Fathom from 'fathom-client'
+// used for rendering equations (optional)
+import 'katex/dist/katex.min.css'
+import posthog from 'posthog-js'
+// used for code syntax highlighting (optional)
+import 'prismjs/themes/prism-coy.css'
+// core styles shared by all of react-notion-x (required)
+import 'react-notion-x/src/styles.css'
+import 'styles/global.css'
+// this might be better for dark mode
+// import 'prismjs/themes/prism-okaidia.css'
+// global style overrides for notion
+import 'styles/notion.css'
+// global style overrides for prism theme (optional)
+import 'styles/prism-theme.css'
+
+import { bootstrap } from '@/lib/bootstrap-client'
+import {
+  fathomConfig,
+  fathomId,
+  isServer,
+  posthogConfig,
+  posthogId
+} from '@/lib/config'
+
+if (!isServer) {
+  bootstrap()
+}
+
+export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter()
+
+  React.useEffect(() => {
+    function onRouteChangeComplete() {
+      if (fathomId) {
+        Fathom.trackPageview()
+      }
+
+      if (posthogId) {
+        posthog.capture('$pageview')
+      }
+    }
+
+    if (fathomId) {
+      Fathom.load(fathomId, fathomConfig)
+    }
+
+    if (posthogId) {
+      posthog.init(posthogId, posthogConfig)
+    }
+
+    router.events.on('routeChangeComplete', onRouteChangeComplete)
+
+    return () => {
+      router.events.off('routeChangeComplete', onRouteChangeComplete)
+    }
+  }, [router.events])
+
+  return ( 
+    <>
+      <BubbleChat
+        chatflowid="bb0bf4ac-25ba-424d-85e1-f68d9a9f9b19"
+        apiHost="https://litlaw-flowise.moodmnky.com"
+        theme={{
+          button: {
+            backgroundColor: "#193F4E",
+            right: 20,
+            bottom: 20,
+            size: "medium",
+            iconColor: "white",
+            customIconSrc: "https://cdn.discordapp.com/attachments/1083532452347269220/1192490146281574442/litlaw-avatar-headshot2.png",
+          },
+          chatWindow: {
+            welcomeMessage: "Welcome back Starr! How can I be of service?",
+            backgroundColor: "#2F3437",
+            height: 700,
+            width: 400,
+            fontSize: 16,
+            poweredByTextColor: "#2F3437",
+            botMessage: {
+              backgroundColor: "#2F3437",
+              textColor: "#FFFFFF",
+              showAvatar: true,
+              avatarSrc: "https://cdn.discordapp.com/attachments/1083532452347269220/1192490146281574442/litlaw-avatar-headshot2.png",
+            },
+            userMessage: {
+              backgroundColor: "#539997",
+              textColor: "#ffffff",
+              showAvatar: true,
+              avatarSrc: "https://raw.githubusercontent.com/zahidkhawaja/langchain-chat-nextjs/main/public/usericon.png",
+            },
+            textInput: {
+              placeholder: "Type your question",
+              backgroundColor: "#2F3437",
+              textColor: "#ffffff",
+              sendButtonColor: "#193F4E",
+            }
+          }
+        }}
+      />
+  <Component {...pageProps} />
+  </>
+  );
+}
